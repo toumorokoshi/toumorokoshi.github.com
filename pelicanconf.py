0b0f6ce4c8e05f425c196e208b8aaaea3c7d5b6a
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2013-03-23 23:49:23 -0700

    adding more to the pelican configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,18 +2,22 @@
 # -*- coding: utf-8 -*- #
 
 AUTHOR = u'Yusuke Tsutsumi'
+AUTHOR_BIO = \
+"""
+Love Python, Love Emacs, and starting to like Golang. I work at Zillow.
+"""
 SITENAME = u'toumorokoshi.github.com'
 SITEURL = ''
+TAGLINE = 'Yusuke Tsutsumi'
+THEME = "pelican-svbtle"
 
 TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
 # Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (('GitHub', 'https://github.com/toumorokoshi'),
+          ('StackOverflow', 'http://stackoverflow.com/users/288570/drakeanderson'))
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
