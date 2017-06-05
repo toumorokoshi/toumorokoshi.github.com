fa096b4538f1d883f975cf9fe603bb932743643d
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2017-06-04 22:26:57 -0700

    adding some notebooks.

---
+++ notebooks/ubuild.py
@@ -0,0 +1,8 @@
+def main(build):
+    build.packages.install("aiohttp")
+    build.packages.install("attrs")
+    build.packages.install("async-timeout")
+    build.packages.install("flask")
+    build.packages.install("matplotlib")
+    build.packages.install("plotly")
+    build.packages.install("jupyter")
