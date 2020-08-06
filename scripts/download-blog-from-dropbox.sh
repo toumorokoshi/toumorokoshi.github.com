9bb043087ad1636fb2cdfb42df4c9a5c184c2cad
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-08-05 22:53:13 -0700

    Adding a step to download blog posts from dropbox

---
+++ scripts/download-blog-from-dropbox.sh
@@ -0,0 +1,6 @@
+# download blog posts from dropbox
+curl -X POST https://content.dropboxapi.com/2/files/download_zip \
+  --header "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
+  --header 'Dropbox-API-Arg: {"path":"/docs/blog"}' --output /tmp/blog.zip
+unzip -o /tmp/blog.zip
+cp -r blog/* .
