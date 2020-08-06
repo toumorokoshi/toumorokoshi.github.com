d73b930f0e7de79352787525ed03099b02123b1d
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-08-05 23:07:52 -0700

    github-actions: removing blog directory
    sometimes older files get caught in there. best to remove them.

--- scripts/download-blog-from-dropbox.sh
+++ scripts/download-blog-from-dropbox.sh
@@ -4,3 +4,4 @@ curl -X POST https://content.dropboxapi.com/2/files/download_zip \
   --header 'Dropbox-API-Arg: {"path":"/docs/blog"}' --output /tmp/blog.zip
 unzip -o /tmp/blog.zip
 cp -rv blog/* .
+rm -r blog
