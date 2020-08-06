5ca8a2db07de8ac86ac678e3c2fa712df41b4284
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2020-08-05 22:57:49 -0700

    github actions: making cp verbose to make it easier to debug

--- scripts/download-blog-from-dropbox.sh
+++ scripts/download-blog-from-dropbox.sh
@@ -3,4 +3,4 @@ curl -X POST https://content.dropboxapi.com/2/files/download_zip \
   --header "Authorization: Bearer $DROPBOX_ACCESS_TOKEN" \
   --header 'Dropbox-API-Arg: {"path":"/docs/blog"}' --output /tmp/blog.zip
 unzip -o /tmp/blog.zip
-cp -r blog/* .
+cp -rv blog/* .
