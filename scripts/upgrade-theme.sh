eed2390f2b995e5b3b960e176e6dd5aa06927f5f
toumorokoshi <toumorokoshi@users.noreply.github.com>
2022-05-07 20:33:30 +0000

    deploy: e8e6ee0a3a80cce39113da2a575c8b04cd1457c0

---
+++ scripts/upgrade-theme.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# upgrade the minima theme
+rm -rf /tmp/minima
+git clone https://github.com/jekyll/minima.git /tmp/minima
+for dir in "_includes" "_layouts" "_sass" "assets"
+do
+    cp -r /tmp/minima/$dir/* $dir
+done
+# files to keep beacause they're heavily modified
+git checkout _includes/disqus_comments.html
+git checkout _includes/header.html
+git checkout _includes/footer.html
+git checkout _layouts/post.html
