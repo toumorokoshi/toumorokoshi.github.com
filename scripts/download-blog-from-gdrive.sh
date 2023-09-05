45db615126d81ab826b96efba8441b30709f7510
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2023-09-04 23:14:39 -0700

    remove pip install

--- scripts/download-blog-from-gdrive.sh
+++ scripts/download-blog-from-gdrive.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export FOLDER_ID="1vOWBuAlm1z3Z53cO6C4HXw7fs76shk98"
-which gdown || pip install --user gdown
+which gdown || pip install gdown
 gdown "https://drive.google.com/drive/folders/$FOLDER_ID" --folder -O .
