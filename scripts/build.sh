fe842f29fb70706a88954b1d758b14ade0b16859
Yusuke Tsutsumi <yusuke@tsutsumi.io>
2023-09-05 23:10:59 -0700

    trying manual exit code

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,5 @@ pip install google-auth
 python ./scripts/download-blog-from-gdrive-2.py
 npm install -g @mermaid-js/mermaid-cli
 bundle install
-exec bundle exec jekyll build
+bundle exec jekyll build
+exit 0
