16fe877837d7c094f0f7be2cc1b5a0a3081727f0
toumorokoshi <toumorokoshi@users.noreply.github.com>
2023-11-28 07:10:57 +0000

    deploy: 5d1dff98e816dbf371b639652f7609031b5f7c92

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ set -x
 python ./scripts/download-blog-from-gdrive-2.py
 #    npm install -g @mermaid-js/mermaid-cli
 bundle install
-bundle exec jekyll build
+JEKYLL_ENV=production bundle exec jekyll build
 exit 0
